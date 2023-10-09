1dbf137b78f7edbda6ca249df56082ddd5a709ac
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2023-10-10 00:18:10 +0400

    added supertab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,28 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp_action.luasnip_supertab(),
+		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+	}),
+})
 
 lsp.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {'tsserver', 'emmet_language_server',
-  'pylsp', 'html', 'jsonls', 'cssls'},
-  handlers = {
-    lsp.default_setup,
-  }
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	-- Replace the language servers listed here
+	-- with the ones you want to install
+	ensure_installed = { "tsserver", "emmet_language_server", "pylsp", "html", "jsonls", "cssls" },
+	handlers = {
+		lsp.default_setup,
+	},
 })
 
 lsp.setup()
