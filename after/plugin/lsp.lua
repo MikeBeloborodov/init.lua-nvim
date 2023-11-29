51a0e9378487544772a6c9b56d881f9a883c275d
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2023-11-29 10:54:45 +0400

    added prisma lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,7 +20,16 @@ require("mason").setup({})
 require("mason-lspconfig").setup({
 	-- Replace the language servers listed here
 	-- with the ones you want to install
-	ensure_installed = { "tsserver", "emmet_language_server", "pylsp", "html", "jsonls", "cssls", "rust_analyzer" },
+	ensure_installed = {
+		"tsserver",
+		"emmet_language_server",
+		"pylsp",
+		"html",
+		"jsonls",
+		"cssls",
+		"rust_analyzer",
+		"prismals",
+	},
 	handlers = {
 		lsp.default_setup,
 	},
